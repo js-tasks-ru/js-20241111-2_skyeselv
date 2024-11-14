737a5715fc3d903721afd43e7a2080fce07b37fb
Ivan Selishchev <selishcheviv@gmail.com>
2024-11-14 09:35:21 +0300

    Fixed issue with unsaved changes

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  return Object.fromEntries(
+    Object.entries(obj).filter(([value]) => fields.indexOf(value) == -1)
+  );
 };
